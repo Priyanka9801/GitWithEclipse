9a46cba682654bce3d46d2bfd8eb9b933aa019ef
priyanka <arshanapelli664@gmail.com>
2022-12-26 23:25:53 +0530

    Initial

--- First/src/main/java/Serv/LoginService.java
+++ First/src/main/java/Serv/LoginService.java
@@ -3,7 +3,7 @@ package Serv;
 
 public class LoginService{
 	
-	
+	//hjgyu
 		public boolean loginCheck(String username, String password) {
 			if(username.equals("priyanka") && password.equals("123"))
 				return true;
